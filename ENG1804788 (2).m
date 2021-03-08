97271063c91c38babd69c94418048efa700ffdf2
lulublake <80258488+lulublake@users.noreply.github.com>
2021-03-08 04:13:14 -0800

    Added file via uploads

---
+++ ENG1804788 (2).m
@@ -0,0 +1,31 @@
+%NAME: ONYEGUKE OLUCHI PRAISE
+%DEPARTMENT: COMPUTER ENGINEERING
+%MARIC NUMBER: ENG1804788
+%COURSE CODE: ECP281
+%DATE: 7TH MARCH, 2021.
+
+% QUESTION 1
+a = 1;
+b = 1;
+c = 1;
+x = 1.6;
+
+d = sqrt(b^2 - 4*a*c); 
+
+x1 = (-b + d)/(2*a);
+x2 = (-b - d)/(2*a);
+
+
+
+% QUESTION 1a
+disp( a*(x*2) + b*x + c );
+
+% QUESTION 1b
+disp([x1 x2]);
+
+
+
+% QUESTION 2
+A = [1 2 3; 1 5 0; 0.5 1.5 4];
+B = [6;6;6];
+disp (A/B)
